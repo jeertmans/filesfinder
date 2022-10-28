eae58b935d1c00bc1d137014eca1e61bdeb4db25
Jérome Eertmans <jeertmans@icloud.com>
2022-10-28 11:30:37 +0200

    fix(ci): releasing 0.3.2

--- Dockerfile
+++ Dockerfile
@@ -1,5 +1,5 @@
 # Container image that runs your code
-FROM jeertmans/filesfinder:0.3.1
+FROM jeertmans/filesfinder:v0.3.2
 
 # Copies your code file from your action repository to the filesystem path `/` of the container
 COPY entrypoint.sh /entrypoint.sh
--- list_versions.py
+++ list_versions.py
@@ -9,14 +9,18 @@ PATTERN_1 = re.compile('filesfinder"\nversion = "(.*)"')
 PATTERN_2 = re.compile("filesfinder[:@]v(.*)")
 
 PATTERNS = [PATTERN_1, PATTERN_2]
-EXTS = ["rs", "toml", "yml", "lock"]
+GLOBS = ["*.rs", "*.toml", "*.yml", "*.lock", "*.md", "Dockerfile"]
 
 if __name__ == "__main__":
-    for ext in EXTS:
-        for file in Path(".").glob(f"**/*.{ext}"):
+    for glob in GLOBS:
+        for file in Path(".").glob(f"**/{glob}"):
             with file.open() as f:
-                text = f.read()
+                try:
+                    text = f.read()
 
-                for pattern in PATTERNS:
-                    for m in pattern.finditer(text):
-                        print(m[1])
+                    for pattern in PATTERNS:
+                        for m in pattern.finditer(text):
+                            print(f"v{m[1]}")
+
+                except Exception as e:
+                    raise Exception(f"Some exeption has occurend when reading file {file}: {str(e)}")
