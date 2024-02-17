2db55689959e0d22a180e25cee2dfceac4b09165
Jérome Eertmans <jeertmans@icloud.com>
2024-02-17 15:35:59 +0100

    fixes

--- tests/same_as_find.rs
+++ tests/same_as_find.rs
@@ -1,6 +1,9 @@
 use std::collections::HashSet;
 use std::process::Command;
 
+const REPOS_DIR: &'static str = concat!(env!("CARGO_MANIFEST_DIR"), "/repos");
+const FF_BIN: &'static str = env!("CARGO_BIN_EXE_ff");
+
 fn stdout_to_paths_set(stdout: Vec<u8>) -> HashSet<String> {
     String::from_utf8(stdout)
         .unwrap()
@@ -19,14 +22,14 @@ macro_rules! command {
 #[macro_export]
 macro_rules! ff {
     ($( $args: expr ),* ) => {
-        command!("ff", "--hidden", "--no-gitignore", "--no-ignore", "--no-strip-prefix", $($args),*)
+        command!(FF_BIN, "-d", REPOS_DIR, "--hidden", "--no-gitignore", "--no-ignore", "--no-strip-prefix", $($args),*)
     };
 }
 
 #[macro_export]
 macro_rules! find {
     ($( $args: expr ),* ) => {
-        command!("find", $($args),*)
+        command!("find", REPOS_DIR, $($args),*)
     };
 }
 
@@ -52,12 +55,12 @@ macro_rules! assert_same_output {
 
 #[test]
 fn test_one_glob_pattern() {
-    assert_same_output!(ff!["*.rs"], find![".", "-wholename", "*.rs"]);
-    assert_same_output!(ff!["*.toml"], find![".", "-wholename", "*.toml"]);
+    assert_same_output!(ff!["*.rs"], find!["-wholename", "*.rs"]);
+    assert_same_output!(ff!["*.toml"], find!["-wholename", "*.toml"]);
 }
 
 #[test]
 fn test_one_regex_pattern() {
-    assert_same_output!(ff!["-r", r".*\.c$"], find![".", "-regex", r".*\.c"]);
-    assert_same_output!(ff!["-r", r".*\.h$"], find![".", "-regex", r".*\.h"]);
+    assert_same_output!(ff!["-r", r".*\.c$"], find!["-regex", r".*\.c"]);
+    assert_same_output!(ff!["-r", r".*\.h$"], find!["-regex", r".*\.h"]);
 }
